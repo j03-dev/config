245946115b86078389b4d136d1d2e35895d4bf53
j03-dev <24nomeniavo@gmail.com>
2022-08-24 02:33:47 +0300

    first commit

---
+++ fish/config.fish
@@ -0,0 +1,69 @@
+if status is-interactive
+and not set -q TMUX
+    exec tmux
+end
+
+# enable vim mode
+fish_vi_key_bindings
+
+export PATH="$PATH:/home/j03/.cargo/bin/"
+
+
+#configure my promt fish
+#### Enable the time to be displayed.
+set -g theme_display_time yes
+
+#### Disable playing the user's current group.
+set -g theme_display_group yes
+
+### Display the system hostname.
+set -g theme_display_hostname yes
+
+#### Disable Git-awareness.
+set -g theme_display_git yes
+
+#### Don't disable jobs indicator.
+set -g theme_display_jobs no
+
+#### Always display the jobs indicator, even if there are no jobs.
+set -g theme_display_jobs_always yes
+
+#### Hide the current directory read/write indicator.
+set -g theme_display_rw no
+
+#### Don't display the VirtualEnv prompt.
+set -g theme_display_virtualenv yes
+
+#### Display the battery
+set -g theme_display_batt yes
+set -g theme_display_batt_icon no
+
+
+set theme_primary                                   1eb980
+set theme_secondary                                 ffcf44
+set theme_primary_variant                           045d56
+set theme_secondary_variant                         ff6859
+set theme_hilight                                   b15dff
+
+set -g theme_color_user                             $theme_hilight
+set -g theme_color_host                             $theme_primary_variant
+set -g theme_color_separator                        brblack
+set -g theme_color_normal                           normal
+set -g theme_color_time                             $theme_secondary_variant
+set -g theme_color_path                             $theme_primary
+set -g theme_color_prompt                           $theme_secondary_variant
+set -g theme_color_virtualenv                       $theme_secondary
+set -g theme_color_status_prefix                    $theme_hilight
+set -g theme_color_status_jobs                      $theme_primary
+set -g theme_color_status_rw                        $theme_primary
+set -g theme_display_group                          no
+set -g theme_prompt_segment_separator_color         $theme_primary
+set -g theme_prompt_userhost_separator              '.'
+set -g __fish_git_prompt_char_branch_begin          '['
+set -g __fish_git_prompt_char_branch_end            ']'
+set -g __fish_git_prompt_color_branch_begin         brblack
+set -g __fish_git_prompt_color_branch_end           brblack
+set -g __fish_git_prompt_color_branch               $theme_secondary
+
+set -gx fish_prompt_pwd_dir_length                  1
+set -g theme_display_jobs_always                    yes
---
+++ nvim/init.vim
@@ -0,0 +1,82 @@
+syntax on
+set noerrorbells
+set tabstop=4 softtabstop=4
+set shiftwidth=4
+set expandtab
+set smartindent
+set nu
+set nowrap
+set smartcase
+set noswapfile
+set nobackup
+set undodir=~/.nvim/undodir
+set undofile
+set incsearch
+set mouse=a
+set relativenumber
+set clipboard=unnamed
+set termguicolors
+
+""" key map setting
+map S : source ~/.config/nvim/init.vim <CR>
+map T : term <CR>
+" map T: Telescope finde_files <CR>
+map B : !python3 /mnt/D/Project/Pybuild/pybuild.py -f % <CR>
+map E : NERDTree <CR>
+map L : !clear <CR>
+map & : mksession! ~/.mysession.vim <CR>
+map ^ : source ~/.mysession.vim <CR>
+map F : Files <CR>
+
+
+call plug#begin('~/.nvim/plugin')
+
+Plug 'vim-python/python-syntax'
+Plug 'ycm-core/YouCompleteMe'
+Plug 'glench/vim-jinja2-syntax'
+Plug 'junegunn/fzf', { 'do': { -> fzf#install() } }
+Plug 'junegunn/fzf.vim'
+Plug 'nvim-lua/plenary.nvim'
+Plug 'nvim-telescope/telescope.nvim', { 'tag': '0.1.0' }
+Plug 'sindrets/diffview.nvim'
+Plug 'preservim/nerdcommenter'
+Plug 'lukas-reineke/indent-blankline.nvim'
+Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}
+
+" Color shemes
+Plug 'morhetz/gruvbox'
+Plug 'https://github.com/arzg/vim-colors-xcode'
+Plug 'https://github.com/NLKNguyen/papercolor-theme'
+Plug 'dracula/vim', { 'name': 'dracula' }
+
+Plug 'vim-utils/vim-man'
+" Plug 'https://github.com/ctrlpvim/ctrlp.vim'
+Plug 'terryma/vim-multiple-cursors'
+Plug 'https://github.com/Townk/vim-autoclose.git'
+Plug 'vim-airline/vim-airline'
+Plug 'vim-airline/vim-airline-themes'
+Plug 'https://github.com/preservim/nerdtree'
+Plug 'ryanoasis/vim-devicons'
+
+call plug#end()
+
+let g:python_highlight_all=1
+" colorscheme setting
+set background=light
+set t_Co=256
+set cursorline
+colorscheme gruvbox
+let g:airline_theme='solarized'
+filetype plugin on
+
+fun! TrimWhitespace()
+	let l:save = winsaveview()
+	keeppatterns %s/\s\+$//e
+	call winrestview(l:save)
+endfun
+
+augroup autoTrimWhitespace
+	autocmd!
+	autocmd! BufWritePre * :call TrimWhitespace()
+augroup END
+
