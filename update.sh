969c17601d4239dc100717e11d430533999abaeb
j03-dev <24nomeniavo@gmail.com>
2024-05-31 19:41:44 +0300

    update config file

--- update.sh
+++ update.sh
@@ -2,7 +2,8 @@
 
 cp ~/.tmux.conf .
 
-config=(i3/ fish/ polybar/ helix/)
+config=(i3/ fish/ helix/ fastfetch/ libinput-gestures.conf picom.conf)
+
 for conf in ${config[@]}; do
     cp -r ~/.config/$conf .
 done
